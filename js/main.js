b1b6ae0172beb1e8242f0dd15d4020a0dbe9c17d
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-11-22 16:23:48 -0500

    main screen is ready, plus mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-$(".menu__btn");
+const hamburgerBtn = document.querySelector(".menu__btn");
+const menuList = document.querySelector(".menu__list");
+
+hamburgerBtn.addEventListener("click", function () {
+  menuList.classList.toggle("menu__list-active");
+});
