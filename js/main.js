1560e49d8434ed116995a41c5a797573de12ddf4
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-11-25 11:16:14 -0500

    reviews slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ const menuList = document.querySelector(".menu__list");
 hamburgerBtn.addEventListener("click", function () {
   menuList.classList.toggle("menu__list-active");
 });
+
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
