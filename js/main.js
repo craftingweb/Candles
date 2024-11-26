430ac360507d94cc6b24cf6b0790a7501eb20166
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-11-25 21:11:44 -0500

    accordeon & mobile friendly

--- js/main.js
+++ js/main.js
@@ -21,3 +21,23 @@ const swiperTwo = new Swiper(".cetrificates__slider", {
     el: ".swiper-pagination",
   },
 });
+
+const accordeon = document.querySelector(".accordeon");
+const accordeonTitles = document.querySelectorAll(".accordeon__title");
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+  accordeonTitle.addEventListener("click", function () {
+    const currentText =
+      accordeonTitle.parentElement.querySelector(".accordeon__text");
+    accordeonTitle.classList.toggle("accordeon__title--active");
+    currentText.classList.toggle("accordeon__text--visible");
+
+    if (currentText.classList.contains("accordeon__text--visible")) {
+      currentText.style.maxHeight = currentText.scrollHeight + "px";
+      currentText.style.opacity = 1;
+    } else {
+      currentText.style.maxHeight = null;
+      currentText.style.opacity = 0;
+    }
+  });
+});
